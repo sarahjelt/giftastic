0079867d4d50b80000d30cf13749f428069c3804
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-10-01 23:29:58 -0400

    base topics array displays at top of page as buttons, clicking each displays 10 gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-var butts = [];
+var topics = ["frodo baggins", "samwise gamgee", "meriadoc brandybuck", "peregrin took", "gandalf", "gollum", "aragorn", "gimli", "legolas", "boromir"];
+
 // on clicking buttons with search terms load gifs and rating
 function renderGifs() {
   console.log("running");
@@ -18,7 +19,11 @@ function renderGifs() {
       var rating = results[i].rating;
       var p = $("<p>").text("Rating: " + rating);
       var img = $("<img>");
-      img.attr("src", results[i].images.fixed_height.url);
+      img.attr("src", results[i].images.fixed_height_still.url);
+      img.attr("data-still", results[i].images.fixed_height_still.url);
+      img.attr("data-animate", results[i].images.fixed_height.url);
+      img.attr("data-state", "still");
+      img.addClass("gif");
       gifDiv.prepend(p);
       gifDiv.prepend(img);
       $("#gifs-go-here").prepend(gifDiv);
@@ -26,38 +31,40 @@ function renderGifs() {
   })
 };
 
-//renders new button with latest gif buttons array push
+//renders new button with latest topics array push
 function renderButtons() {
   $("#buttons").empty();
-  for (var i = 0; i < butts.length; i++) {
+  for (var i = 0; i < topics.length; i++) {
     var newButton = $("<button>");
     newButton.addClass("new-button");
-    newButton.attr("data-img", butts[i]);
-    newButton.text(butts[i]);
+    newButton.attr("data-img", topics[i]);
+    newButton.text(topics[i]);
     $("#buttons").append(newButton);
   }
 }
 
-//on click adds new button to gif buttons array
+//on click, adds new button to gif buttons array
 $("#add-new-button").on("click", function(event) {
   event.preventDefault();
   var searchTerm = $("#form-text").val().trim();
-  butts.push(searchTerm);
+  topics.push(searchTerm);
   renderButtons();
   })
 
-//trigger still or animated -- need to figure out data attributes on images and diff links and stuff
-// $("img").on("click", function () {
-//   var state = $(this).attr("data-state");
-//   console.log(state);
+//trigger still or animated
+$(document).on("click", ".gif", function () {
+  var state = $(this).attr("data-state");
+  console.log(state);
 
-//   if (state === "still") {
-//     $(this).attr("data-state", "animate");
-//     $(this).attr("src", $(this).attr("data-animate"));
-//   } else {
-//     $(this).attr("data-state", "still");
-//     $(this).attr("src", $(this).attr("data-still"));
-//   }
-// })
+  if (state === "still") {
+    $(this).attr("data-state", "animate");
+    $(this).attr("src", $(this).attr("data-animate"));
+  } else {
+    $(this).attr("data-state", "still");
+    $(this).attr("src", $(this).attr("data-still"));
+  }
+})
+
+renderButtons();
 
 $(document).on("click", "button", renderGifs);
